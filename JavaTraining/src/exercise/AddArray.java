dc1b2c8d7d9f611c4c37475906833cb8bfd51d0c
lokesh62 <lokeshpentyala1999@gmail.com>
2022-12-30 19:47:14 -0800

    adding files

---
+++ JavaTraining/src/exercise/AddArray.java
@@ -0,0 +1,11 @@
+package exercise;
+
+import java.util.Arrays;
+
+public class AddArray {
+    public static void main(String []args){
+        int arr1[] = {1,3,5};
+        int arr2[] = {2,4,7,9};
+        System.out.println(Arrays.toString(arr1)+Arrays.toString(arr2));
+    }
+}
